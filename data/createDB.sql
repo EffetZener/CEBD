cbd811df31e9ba36917e3f0ee7e8634a9add2952
EffetZéner <nathan.leboucher73@gmail.co>
2020-12-03 08:27:05 +0100

    ajout de views

--- data/createDB.sql
+++ data/createDB.sql
@@ -76,6 +76,34 @@ CREATE VIEW LesEquipes AS
     FROM LesEquipiers 
     GROUP BY numEq;
 
+CREATE VIEW LesClassements AS
+    SELECT R.numEp AS numEp, S1.pays AS P1, S2.pays AS P2, S3.pays AS P3 
+    FROM LesResultats R JOIN LesSportifs S1 JOIN LesSportifs S2 JOIN LesSportifs S3 
+    ON S1.numSp = gold AND S2.numSp = silver AND S3.numSp = bronze
+    UNION
+    SELECT DISTINCT R.numEp AS numEp, S1.pays AS P1, S2.pays AS P2, S3.pays AS P3  
+    FROM LesResultats R JOIN LesEquipiers  E1 JOIN LesEquipiers  E2 JOIN LesEquipiers E3 JOIN LesSportifs S1 JOIN LesSportifs S2 JOIN LesSportifs S3
+    ON E1.numEq = gold AND E2.numEq = silver AND E3.numEq = bronze AND E1.numSp = S1.numsp AND E2.numSp = S2.numsp AND E3.numSp = S3.numsp;
+
+CREATE VIEW paysOrs AS 
+    SELECT count(numEp) AS nbOr,P1 AS pays FROM LesClassements GROUP BY P1;
+
+CREATE VIEW paysArgents AS 
+    SELECT count(numEp) AS nbArgent,P2 AS pays FROM LesClassements GROUP BY P2;
+
+CREATE VIEW paysBronzes AS 
+    SELECT count(numEp) AS nbBronze,P3 AS pays FROM LesClassements GROUP BY P3;
+
+
+CREATE VIEW paysMed AS
+    SELECT pays as pays ,nbOr as nbOr,0 as nbArgent,0 as nbBronze
+    FROM paysOrs 
+        UNION 
+    SELECT pays,0,nbArgent,0 
+    FROM paysArgents 
+        UNION
+    SELECT pays,0,0,nbBronze
+    FROM paysBronzes;
 -- TODO 1.2a : ajouter la définition de la vue LesSportifs
 -- TODO 1.3a : ajouter la création de la table LesDisciplines et ajouter l'attribut discipline dans la table LesEpreuves
 -- TODO 1.4a : ajouter la définition de la vue LesEquipes
